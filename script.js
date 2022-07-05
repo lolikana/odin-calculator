34de2518e5ae8b0dc5cf145bd9b2332129742d5b
lolikana <thomas.guiart@gmail.com>
2022-07-05 21:08:21 +0900

    change text bottom to top

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 const para = document.createElement('p');
+const displaybottom = document.getElementById('display-bottom');
 const displayTop = document.getElementById('display-top');
-const displayBottom = document.getElementById('display-bottom');
 const selectNumberBtn = document.getElementsByClassName('number');
 const selectDotBtn = document.getElementById('dot');
 const selectNegativeBtn = document.getElementsByClassName('control__negative');
 const selectClearBtn = document.getElementsByClassName('control__clear');
 const selectDeleteBtn = document.getElementsByClassName('control__delete');
+const selectOperatorBtn = document.getElementsByClassName('operator');
 
-let textTopArray = [];
-let textBottomArray = [0];
+let textTopArray = [0];
+let textBottomArray = [];
 
-displayBottom.textContent = textBottomArray;
+displayTop.textContent = textTopArray;
 
 function displayText() {
-  displayBottom.textContent = textBottomArray.join('');
+  displayTop.textContent = textTopArray.join('');
 }
 
 for (let i = 0; i < selectNumberBtn.length; i++) {
@@ -21,48 +22,50 @@ for (let i = 0; i < selectNumberBtn.length; i++) {
     'click',
     (btnValue = () => {
       const value = selectNumberBtn[i].innerHTML;
-      if (textBottomArray[0] === 0 && value !== '.' && textBottomArray.length === 1) {
-        textBottomArray.splice(0, 1, value);
+      if (textTopArray[0] === 0 && value !== '.' && textTopArray.length === 1) {
+        textTopArray.splice(0, 1, value);
         checkDotBtn();
         displayText();
       } else {
-        textBottomArray.push(value);
+        textTopArray.push(value);
         checkDotBtn();
         displayText();
       }
+      console.log(textTopArray);
     })
   );
 }
 
 selectNegativeBtn[0].addEventListener('click', () => {
-  if (textBottomArray[0] !== '-') {
-    textBottomArray.unshift('-');
-  } else if (textBottomArray[0] === '-') {
-    textBottomArray.shift();
+  if (textTopArray[0] !== '-') {
+    textTopArray.unshift('-');
+  } else if (textTopArray[0] === '-') {
+    textTopArray.shift();
   }
+  console.log(textTopArray);
   return displayText();
 });
 
 function clearDisplay() {
-  textBottomArray = [0];
+  textTopArray = [0];
   displayText();
   checkDotBtn();
 }
 selectClearBtn[0].addEventListener('click', clearDisplay);
 
 function deleteLastNumber() {
-  textBottomArray.pop();
+  textTopArray.pop();
   displayText();
   checkDotBtn();
-  if (textBottomArray.length === 0) {
-    textBottomArray = [0];
+  if (textTopArray.length === 0) {
+    textTopArray = [0];
     displayText();
   }
 }
 selectDeleteBtn[0].addEventListener('click', deleteLastNumber);
 
 function checkDotBtn() {
-  if (textBottomArray.includes('.')) {
+  if (textTopArray.includes('.')) {
     selectDotBtn.classList.add('disabled');
     selectDotBtn.setAttribute('disabled', '');
   } else {
